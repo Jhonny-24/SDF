e89652d1205aa994a0d8b8760f11274805d55d27
Lorezno00 <lorenzo.livorsi@gmail.com>
2025-05-21 18:28:22 +0200

    Update sketch.js

--- assets/sketches/Lab p5js cerchio/sketch.js
+++ assets/sketches/Lab p5js cerchio/sketch.js
@@ -5,16 +5,21 @@ function setup() {
 function draw() {   
 
   background(0); 
-
+  
   stroke(255)
   noFill();
-  strokeWeight(2);
-
+  
   const DimStatic = min(400,windowHeight/1.8) ;
   const raggStatic = DimStatic / 2;
+  const ValoreResponsivo = raggStatic*0.07
   const centroStaticX = min(1200/2, windowWidth / 2);
   const centroStaticY = min(1200/2, windowHeight / 2);
-
+  if (ValoreResponsivo>2){
+     strokeWeight(ValoreResponsivo*0.15);
+  } else {
+    strokeWeight(ValoreResponsivo*0.20);
+  }
+ 
   ellipse(centroStaticX, centroStaticY, DimStatic);
   
   const distanza = dist(mouseX, mouseY, centroStaticX, centroStaticY);
@@ -22,7 +27,7 @@ function draw() {
   const centroDinamic = (mouseX, mouseY)
   const raggDinamic = abs(distanza - raggStatic);
   const DimDinamic = raggDinamic * 2;
-  
+
   //da rivdere da qui in poi 
   const CentroX = centroStaticX - mouseX
   const CentroY = centroStaticY - mouseY
@@ -58,18 +63,31 @@ function draw() {
 
   stroke(255,255, 0)
   ellipse(mouseX, mouseY, DimDinamic);
-
+if (ValoreResponsivo >2){
+  b = ValoreResponsivo*1.8
+} else if (ValoreResponsivo>10){
+  b = ValoreResponsivo*0.5
+}
   noStroke()
   fill(255, 255, 0)
-  ellipse(mouseX, mouseY, raggStatic*0.1)
+  ellipse(mouseX, mouseY, b)
 
   fill(255); 
   textSize(14);
   textAlign(CENTER, CENTER);
 
 
-  const greenLineMidX = (mouseX + lineEndX) / 2;
-  const greenLineMidY = (mouseY + lineEndY) / 2;
-  text(raggDinamic.toFixed(0), greenLineMidX, greenLineMidY);
-}
+  //text(raggDinamic.toFixed(0), greenLineMidX, greenLineMidY);
+  //const greenLineMidY = (mouseY + lineEndY) / 2;
+  //const greenLineMidX = (mouseX + lineEndX) / 2;
 
+  if (distanza < raggStatic) {
+    textSize(ValoreResponsivo*2.5);
+    text (-raggDinamic.toFixed(0), mouseX, mouseY- ValoreResponsivo*3) 
+  } else { 
+    textSize(ValoreResponsivo*2.5);
+    text (raggDinamic.toFixed(0), mouseX, mouseY- ValoreResponsivo*3) 
+  }
+  console.log(ValoreResponsivo)
+}
+ 
